d9667afd9652257df35b021c318a1a07d374a737
Jacob <1705287@uad.ac.uk>
2019-03-29 15:52:15 +0000

    Melting Mechanic

--- blob_test/Heat&Cool_Area/Cool_Area.gd
+++ blob_test/Heat&Cool_Area/Cool_Area.gd
@@ -1,13 +1,33 @@
 extends Spatial
 
+var show_state
+var show_time
+var show_timer
+
 func _ready():
 	$Trigger.connect("area_entered", self, "trigger_area_entered")
+	show_state = true
+	show_time = 3.0
+	show_timer = 0
 
 func _physics_process(delta):
-	#$MeshInstance.visible = false
+	show_timer += delta
+	if show_timer >= show_time:
+		change_state()
+		show_timer = 0
 	pass
 
 func trigger_area_entered(area):
 	#If player enter
 	if area.has_method("add_score"):
 		get_node("../..").cool()
+
+func change_state():
+	if show_state:
+		show_state = false
+		$MeshInstance.visible = false
+		$Trigger/CollisionShape.disabled = true
+	else:
+		show_state = true
+		$MeshInstance.visible = true
+		$Trigger/CollisionShape.disabled = false
