6469b33ea31ebb8af5c223cf2ccf0f46043ccdbf
Jacob <1705287@uad.ac.uk>
2019-04-17 13:25:45 +0100

    Update spin mechanic
    Player will be slow down in water under normal state. If player in spin state, player will keep normal speed in water and the speed of falling slows after jumping

---
+++ blob_test/Slow_down_area.gd
@@ -0,0 +1,20 @@
+extends Area
+
+var speed_in_water = 0.5
+var speed_normal = 2
+
+func _ready():
+	connect("area_entered", self, "player_area_entered")
+	connect("area_exited", self, "player_area_exited")
+	
+func player_area_entered(area):
+	if area.has_method("add_score"):
+		if get_node("../Player").spin:
+			get_node("../Player").movement_speed = speed_normal
+		else:
+			get_node("../Player").movement_speed = speed_in_water
+
+func player_area_exited(area):
+	if area.has_method("add_score"):
+		get_node("../Player").movement_speed = speed_normal
+
