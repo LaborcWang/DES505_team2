177210b53deb5ca830565abf23b6fa013e90c574
Jacob <1705287@uad.ac.uk>
2019-04-18 14:03:58 +0100

    Update Main Menu
    Can choose main teat level or spin test level in main menu now

--- blob_test/Main_Menu.gd
+++ blob_test/Main_Menu.gd
@@ -1,14 +1,18 @@
 extends Control
 
 export (String, FILE) var main_scene
+export (String, FILE) var spin_scene
 
 func _ready():
-	$Main_Page/Start_Button.connect("pressed", self, "start_pressed")
+	$Main_Page/Main_Level_Button.connect("pressed", self, "main_level_pressed")
 	$Main_Page/Quit_Button.connect("pressed", self, "quit")
+	$Main_Page/Spin_Level_Button.connect("pressed", self, "spin_level_pressed")
 
 func quit():
 	get_tree().quit()
 
-func start_pressed():
+func main_level_pressed():
 	get_node("/root/Globals").load_new_scene(main_scene)
 
+func spin_level_pressed():
+	get_node("/root/Globals").load_new_scene(spin_scene)
